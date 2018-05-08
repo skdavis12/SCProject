bae8071a4d76aa762d979cb0f698607eda6e2012
skdavis12 <38544435+skdavis12@users.noreply.github.com>
2018-05-08 08:08:54 -0500

    added grid convergence test

---
+++ gridconvergencetest.m
@@ -0,0 +1,8 @@
+[Ue_5,~,~,te_5,he_5]=explicit_ss(5,0.001);
+[Ue_10,~,~,te_10,he_10]=explicit_ss(10,0.001);
+[Ue_15,~,~,te_15,he_15]=explicit_ss(15,0.001);
+[Ue_20,~,~,te_20,he_20]=explicit_ss(20,0.001);
+[Ue_25,~,~,te_25,he_25]=explicit_ss(25,0.001);
+[Ue_30,~,~,te_30,he_30]=explicit_ss(30,0.001);
+[Ue_35,~,~,te_35,he_35]=explicit_ss(35,0.001);
+[Ue_40,~,~,te_40,he_40]=explicit_ss(40,0.001);
