eb6fe9f1b81b2e18cfde78b9809d78a43e28bbba
skdavis12 <38544435+skdavis12@users.noreply.github.com>
2018-05-08 09:23:40 -0500

    Add files via upload

--- gridconvergencetest.m
+++ gridconvergencetest.m
@@ -1,8 +1,6 @@
-[Ue_5,~,~,te_5,he_5]=explicit_ss(5,0.001);
-[Ue_10,~,~,te_10,he_10]=explicit_ss(10,0.001);
-[Ue_15,~,~,te_15,he_15]=explicit_ss(15,0.001);
-[Ue_20,~,~,te_20,he_20]=explicit_ss(20,0.001);
-[Ue_25,~,~,te_25,he_25]=explicit_ss(25,0.001);
-[Ue_30,~,~,te_30,he_30]=explicit_ss(30,0.001);
-[Ue_35,~,~,te_35,he_35]=explicit_ss(35,0.001);
-[Ue_40,~,~,te_40,he_40]=explicit_ss(40,0.001);
+[Ue_45,~,~,te_45,he_45]=explicit_ss(45,0.001);
+[Ue_50,~,~,te_50,he_50]=explicit_ss(50,0.001);
+[Ue_55,~,~,te_55,he_55]=explicit_ss(55,0.001);
+[Ue_60,~,~,te_60,he_60]=explicit_ss(60,0.001);
+[Ue_65,~,~,te_65,he_65]=explicit_ss(65,0.001);
+[Ue_70,~,~,te_70,he_70]=explicit_ss(70,0.001);
